d25d43c9b5a32e70650ee4bc4feeb09e72c7e2c2
sukeshun <sukeshun718@icloud.com>
2023-03-13 00:30:00 +0900

    フォームを部分テンプレートに変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only:[:show, :edit, :update]
     def index
       @articles = Article.all
     end
     def show
-      @article = Article.find(params[:id])
     end
     def new
       @article = Article.new
@@ -20,11 +20,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-      @article = Article.find(params[:id])
+
     end
 
     def update
-      @article = Article.find(params[:id])
       if @article.update(article_params)
         redirect_to article_path(@article),notice: '更新できました'
       else
@@ -34,8 +33,8 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-      @article = Article.find(params[:id])
-      @article.destroy!
+      article = Article.find(params[:id])
+      article.destroy!
       redirect_to root_path, notice: '削除に成功しました'
 
     end
@@ -45,4 +44,9 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :content)
     end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
 end
